bb4631f773d1597000a37995b19c86a6e519b955
julianjorgensen <julian@coverallcrew.com>
2013-08-09 18:07:57 -0700

    first commit

--- _assets/javascripts/index.js
+++ _assets/javascripts/index.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
+  // THIS IS SORT OF PLACEHOLDER CODE
+  $("#marquee .nav li:first-child").addClass("active");
+  $("body").addClass("durability");
+
+  // UPDATE CLASSES WHEN USING THE MAIN TABS
+  $("#marquee .nav a").click(function(){
+    $("body").removeClass("durability usability art");
+    $("body").addClass($(this).attr("href").replace('#',''));
+  });
+
+
+  // SMART SCROLL TO NEXT WINDOW WHEN USING WHEEL OR TRACKPAD SCROLL
   $(window).bind('mousewheel', function(event) {
       if (event.originalEvent.wheelDelta >= 0) {
         smartScroll("up");
@@ -10,11 +22,12 @@ $(document).ready(function() {
       }
   });
 
+  // AUTO STICK TO NEAREST WINDOW
   $('body').mouseenter(function() {
     autoScrollToNearestWindow();
   });
 
-
+  // LAZY LOAD CONTENT WHEN SCROLLING (EITHER SCROLLBAR OR WHEEL)
   $(window).scroll(function () {
     if (!$("body").hasClass('lazy-loading-content'))
     {
